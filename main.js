b74cede6879f93b3735e61b2d8d3e3e3bb3ecf23
feden2906 <feden2906@gmail.com>
2024-01-04 22:26:48 +0200

    hw-1

--- main.js
+++ main.js
@@ -1,64 +1,104 @@
-// MODULES
-// const { help } = require('./helpers/helper');
-// help();
+const fs = require('node:fs/promises');
+const path = require('node:path');
 
-// GLOBAL VARIABLES
-// console.log(__dirname, 'dirname');
-// console.log(__filename, 'filename');
-// console.log(process.cwd(), 'process.cwd');
+// const foo = async () => {
+//     const basePath = path.join(process.cwd(), 'base-folder');
+//     try {
+//         await fs.readdir(basePath);
+//         await fs.rm(basePath, { recursive: true, force: true })
+//     } catch (e) {
+//         console.error(e.message);
+//     }
+//     await fs.mkdir(basePath, { recursive: true });
+//
+//     const folders = ['folder1', 'folder2', 'folder3', 'folder4'];
+//     const files = ['file1', 'file2', 'file3', 'file4'];
+//
+//     for (const folder of folders) {
+//         const folderPath = path.join(basePath, folder);
+//         await fs.mkdir(folderPath, { recursive: true });
+//     }
+//     for (const file of files) {
+//         const filePath = path.join(basePath, `${file}.txt`);
+//         await fs.writeFile(filePath, 'Hey!');
+//     }
+//
+//     const filesNames = await fs.readdir(basePath);
+//     for (const filesName of filesNames) {
+//         const stat = await fs.stat(path.join(basePath, filesName));
+//         console.log(filesName, stat.isDirectory())
+//     }
+// }
+// void foo();
+
+const foo = async () => {
+    const basePath = path.join(process.cwd(), 'base-folder');
+    try {
+        await fs.readdir(basePath);
+        await fs.rm(basePath, {recursive: true, force: true})
+    } catch (e) {
+        console.error(e.message);
+    }
+    await fs.mkdir(basePath, {recursive: true});
+
+    const folders = ['folder1', 'folder2', 'folder3', 'folder4'];
+    const files = ['file1', 'file2', 'file3', 'file4'];
+
+    await Promise.allSettled([
+        ...folders.map(async (folder) => {
+            const folderPath = path.join(basePath, folder);
+            await fs.mkdir(folderPath, {recursive: true});
+        }),
+        ...files.map(async (file) => {
+            const filePath = path.join(basePath, `${file}.txt`);
+            await fs.writeFile(filePath, 'Hey!');
+        }),
+    ]);
+
+    const filesNames = await fs.readdir(basePath);
+    for (const filesName of filesNames) {
+        const stat = await fs.stat(path.join(basePath, filesName));
+        console.log(filesName, stat.isDirectory())
+    }
+}
+void foo();
 
 
-// PATH
-const path = require('path');
-// const joinedPath = path.join(__dirname, 'helper', 'helper.js');
-// const resolvedPath = path.resolve('helper', 'helper.js');
-// const normalizedPath = path.normalize('/foo/bar//////baz/asdf/quux/..');
-
-// FS
-const fs = require('fs');
-
-const pathToFile = path.join(__dirname, 'data', 'text.txt');
-const pathToCreatedFile = path.join(__dirname, 'data', 'text2.txt');
-
-// fs.readFile(pathToFile, (err, data)=>{
-//   if (err) throw new Error();
-//   console.log(data.toString());
-// })
-
-// fs.appendFile(pathToFile, 'hello okten', (err)=>{
-//   if (err) throw new Error();
-// })
-
-// fs.writeFile(pathToCreatedFile, 'hello recently created file', (err)=>{
-//   if (err) throw new Error();
-// })
-
-// fs.truncate(pathToCreatedFile, (err)=>{
-//   if (err) throw new Error();
-// })
-
-// fs.unlink(pathToCreatedFile, (err)=>{
-//    if (err) throw new Error();
-//  })
-
-// fs.readdir(path.join(__dirname, 'data'), (err, files)=>{
-//   if (err) throw new Error()
-//   console.log(files)
-// })
-
-// fs.mkdir(path.join(__dirname, 'data3'), (err)=>{
-//   if (err) throw new Error()
-// })
-
-// fs.rmdir(path.join(__dirname, 'data3'), (err)=>{
-//   if (err) throw new Error()
-// })
-
-// fs.unlink(path.join(__dirname, 'main.js'), (err)=>{
-//   if (err) {
-//     throw new Error()
-//   }})
-
-const os = require('os');
-
-console.log(os.type())
+// const foo = async () => {
+//     const basePath = path.join(process.cwd(), 'base-folder');
+//     try {
+//         await fs.readdir(basePath);
+//         await fs.rm(basePath, {recursive: true, force: true})
+//     } catch (e) {
+//         console.error(e.message);
+//     }
+//     await fs.mkdir(basePath, {recursive: true});
+//
+//     const folders = ['folder1', 'folder2', 'folder3', 'folder4'];
+//     // const files = ['file1', 'file2', 'file3', 'file4'];
+//
+//     await Promise.allSettled(
+//         folders.map(async (folder, i) => {
+//             const folderPath = path.join(basePath, folder);
+//             await fs.mkdir(folderPath, {recursive: true});
+//
+//             const filePath = path.join(folderPath, `file${i + 1}.txt`);
+//             await fs.writeFile(filePath, 'Hey!');
+//         }),
+//     );
+//
+//     const filesNames = await fs.readdir(basePath);
+//     for (const filesName of filesNames) {
+//         const folderPath = path.join(basePath, filesName);
+//         const stat = await fs.stat(folderPath);
+//         console.log(filesName, stat.isDirectory());
+//
+//         const filesNames = await fs.readdir(folderPath);
+//
+//         for (const filesName of filesNames) {
+//             const stat = await fs.stat(path.join(folderPath, filesName));
+//             console.log(filesName, stat.isDirectory())
+//         }
+//     }
+// }
+// void foo();
