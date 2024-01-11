fddd0a8ace7e8af44834f2f3be9b432c92e93276
feden2906 <feden2906@gmail.com>
2024-01-11 22:17:38 +0200

    ssr / hbs

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,90 @@
-// HW – закінчити з CRUD операціями.
-//     Винести базу даних в json.file, при створенні записувати туда нових юзерів через fs
-// При створенні валідацію на імя і вік, імя повинно бути більше 3 символів, вік – не менше нуля
-// На гет, пут, деліт юзерів перевірити чи такий юзер є
+import * as path from "node:path";
 
 import express, { Request, Response } from "express";
+import { engine } from "express-handlebars";
 
 import { configs } from "./configs/config";
 import { read, write } from "./fs.service";
+import { IUser } from "./interfaces/user.interface";
 
 const app = express();
+
+let errorState = "";
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(express.static(path.join(process.cwd(), "static")));
+app.set("view engine", ".hbs");
+app.engine(".hbs", engine({ defaultLayout: false }));
+app.set("views", path.join(process.cwd(), "static"));
+
+app.get("/login", async (req: Request, res: Response) => {
+  res.render("login");
+});
+app.post("/login", async (req: Request, res: Response) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !email.includes("@")) {
+      throw new Error("wrong email");
+    }
+
+    const users = await read();
+    const user = users.find((user) => user.email === email);
+    if (!user || user.password !== password) {
+      throw new Error("wrong email or password");
+    }
+
+    res.redirect("users");
+  } catch (e) {
+    res.redirect("/error");
+    errorState = e.message;
+  }
+});
+
+app.get("/register", async (req: Request, res: Response) => {
+  res.render("register");
+});
+app.post("/register", async (req: Request, res: Response) => {
+  try {
+    const { email, userName, password } = req.body;
+    const age = Number(req.body.age);
+    if (!age || !Number.isInteger(age) || age <= 0 || age > 100) {
+      throw new Error("wrong age");
+    }
+    if (!email || !email.includes("@")) {
+      throw new Error("wrong email");
+    }
+    if (!userName || userName.length <= 3) {
+      throw new Error("wrong name");
+    }
+    const users = await read();
+
+    const newUser: IUser = {
+      id: users[users.length - 1].id + 1,
+      userName,
+      email,
+      password,
+      age,
+    };
+    users.push(newUser);
+    await write(users);
+
+    res.render("login");
+  } catch (e) {
+    res.redirect("/error");
+    errorState = e.message;
+  }
+});
+
 app.get("/users", async (req: Request, res: Response) => {
   try {
     const users = await read();
-    res.status(200).json({ data: users });
+    res.render("users", { users });
   } catch (e) {
-    res.status(400).json(e.message);
+    res.redirect("/error");
+    errorState = e.message;
   }
 });
 
@@ -35,129 +101,72 @@ app.get("/users/:id", async (req: Request, res: Response) => {
     if (index === -1) {
       throw new Error("user not found");
     }
-    res.json({ data: users[index] });
+    res.render("chosenUser", { user: users[index] });
   } catch (e) {
-    res.status(400).json(e.message);
+    res.redirect("/error");
+    errorState = e.message;
   }
 });
-
-app.post("/users", async (req: Request, res: Response) => {
-  try {
-    const { email, name, age } = req.body;
-    if (!age || !Number.isInteger(age) || age <= 0 || age > 100) {
-      throw new Error("wrong age");
-    }
-    if (!email || !email.includes("@")) {
-      throw new Error("wrong email");
-    }
-    if (!name || name.length <= 3) {
-      throw new Error("wrong name");
-    }
-    const users = await read();
-
-    const newUser = { id: users[users.length - 1].id + 1, email, name, age };
-    users.push(newUser);
-    await write(users);
-
-    res.status(201).json({ data: newUser });
-  } catch (e) {
-    res.status(400).json(e.message);
-  }
-});
-
-app.delete("/users/:id", async (req: Request, res: Response) => {
-  try {
-    const id = Number(req.params.id);
-
-    if (!Number.isInteger(id)) {
-      throw new Error("wrong ID param");
-    }
-
-    const users = await read();
-    const index = users.findIndex((user) => user.id === id);
-    if (index === -1) {
-      throw new Error("user not found");
-    }
-    users.splice(index, 1);
-    await write(users);
-
-    res.sendStatus(204);
-  } catch (e) {
-    res.status(400).json(e.message);
-  }
-});
-
-app.put("/users/:id", async (req: Request, res: Response) => {
-  try {
-    const id = Number(req.params.id);
-    const { email, age, name } = req.body;
-
-    if (!Number.isInteger(id)) {
-      throw new Error("wrong ID param");
-    }
-
-    if (!age || !Number.isInteger(age) || age <= 0 || age > 100) {
-      throw new Error("wrong age");
-    }
-    if (!email || !email.includes("@")) {
-      throw new Error("wrong email");
-    }
-    if (!name || name.length <= 3) {
-      throw new Error("wrong name");
-    }
-
-    const users = await read();
-    const user = users.find((user) => user.id === id);
-    if (!user) {
-      throw new Error("user not found");
-    }
-    user.name = name;
-    user.age = age;
-    user.email = email;
-
-    await write(users);
-
-    res.status(201).json(user);
-  } catch (e) {
-    res.status(400).json(e.message);
-  }
-});
-
-// app.patch('/users/:id', async (req, res) => {
-//     try {
-//         const id = Number(req.params.id);
-//         const {email, age, name} = req.body;
+// app.delete("/users/:id", async (req: Request, res: Response) => {
+//   try {
+//     const id = Number(req.params.id);
 //
-//         if (!Number.isInteger(id)) {
-//             throw new Error('wrong ID param');
-//         }
-//
-//         if (age && (!age || !Number.isInteger(age) || age <= 0 || age > 100)) {
-//             throw new Error('wrong age');
-//         }
-//         if (email && (!email || !email.includes('@'))) {
-//             throw new Error('wrong email');
-//         }
-//         if (name && (!name || name.length <= 3)) {
-//             throw new Error('wrong name');
-//         }
-//
-//         const users = await read();
-//         const user = users.find((user) => user.id === id);
-//         if (!user) {
-//             throw new Error('user not found');
-//         }
-//         if (name) user.name = name;
-//         if (age) user.age = age;
-//         if (email) user.email = email;
-//
-//         await write(users);
-//
-//         res.status(201).json(user);
-//     } catch (e) {
-//         res.status(400).json(e.message);
+//     if (!Number.isInteger(id)) {
+//       throw new Error("wrong ID param");
 //     }
+//
+//     const users = await read();
+//     const index = users.findIndex((user) => user.id === id);
+//     if (index === -1) {
+//       throw new Error("user not found");
+//     }
+//     users.splice(index, 1);
+//     await write(users);
+//
+//     res.sendStatus(204);
+//   } catch (e) {
+//     res.status(400).json(e.message);
+//   }
 // });
+// app.put("/users/:id", async (req: Request, res: Response) => {
+//   try {
+//     const id = Number(req.params.id);
+//     const { email, age, name } = req.body;
+//
+//     if (!Number.isInteger(id)) {
+//       throw new Error("wrong ID param");
+//     }
+//
+//     if (!age || !Number.isInteger(age) || age <= 0 || age > 100) {
+//       throw new Error("wrong age");
+//     }
+//     if (!email || !email.includes("@")) {
+//       throw new Error("wrong email");
+//     }
+//     if (!name || name.length <= 3) {
+//       throw new Error("wrong name");
+//     }
+//
+//     const users = await read();
+//     const user = users.find((user) => user.id === id);
+//     if (!user) {
+//       throw new Error("user not found");
+//     }
+//     user.name = name;
+//     user.age = age;
+//     user.email = email;
+//
+//     await write(users);
+//
+//     res.status(201).json(user);
+//   } catch (e) {
+//     res.status(400).json(e.message);
+//   }
+// });
+
+app.get("/error", async (req: Request, res: Response) => {
+  res.render("error", { errorState });
+});
 
 const PORT = configs.PORT;
 app.listen(PORT, () => {
--- src/fs.service.ts
+++ src/fs.service.ts
@@ -1,17 +1,16 @@
 import * as fs from "fs/promises";
 import * as path from "path";
 
-// TODO
-// interface IUser {}
+import { IUser } from "./interfaces/user.interface";
 
 const pathToFile = path.join(process.cwd(), "db.json");
 
-const read = async () => {
+const read = async (): Promise<IUser[]> => {
   const json = await fs.readFile(pathToFile, { encoding: "utf-8" });
   return JSON.parse(json);
 };
 
-const write = async (users) => {
+const write = async (users: IUser[]): Promise<void> => {
   await fs.writeFile(pathToFile, JSON.stringify(users));
 };
 
